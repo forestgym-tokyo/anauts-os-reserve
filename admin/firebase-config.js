4e9b0b3f6eee0d95d05ed16264ad47f49fb8b7aa
forestgym-tokyo <info@theforestgym.com>
2026-08-23 19:30:41 +0900

    Load stable monthly schedule extension after admin startup

--- admin/firebase-config.js
+++ admin/firebase-config.js
@@ -5,7 +5,6 @@ window.ANAUTS_AUTH = {
 
 window.addEventListener("load", function () {
   var s = document.createElement("script");
-  s.src = "./admin-v57.js?v=20260823-1";
-  s.defer = true;
+  s.src = "./admin-monthly-v58.js?v=20260823-1929";
   document.body.appendChild(s);
 });
