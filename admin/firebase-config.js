ec9599312a8d416decd2af4d91bcdcb4b435756d
forestgym-tokyo <info@theforestgym.com>
2026-08-25 13:09:20 +0900

    Force latest my-shift fix version

--- admin/firebase-config.js
+++ admin/firebase-config.js
@@ -25,6 +25,6 @@ window.addEventListener("load", function () {
   document.body.appendChild(questionnaireFix);
 
   var myShiftFix = document.createElement("script");
-  myShiftFix.src = "./admin-myshift-fix.js?v=20260825-1305";
+  myShiftFix.src = "./admin-myshift-fix.js?v=20260825-1315";
   document.body.appendChild(myShiftFix);
 });
